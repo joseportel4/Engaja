6a58a048882eec31c9fe3e49590a9ba2447b54ce
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2025-09-03 20:24:05 -0300

    atributo para ativar ou desativar marcação de presenca

--- database/migrations/2025_09_01_155040_create_atividades_table.php
+++ database/migrations/2025_09_01_155040_create_atividades_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->date('dia');
             $table->time('hora_inicio');
             $table->unsignedInteger('carga_horaria');
+            $table->boolean('presenca_ativa')->default(false);
             $table->timestamps();
             $table->softDeletes();
         });
